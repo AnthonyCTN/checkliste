d7106d4bc49434c18d5521f558179c27abef3a4c
AnthonyCTN <135627741+AnthonyCTN@users.noreply.github.com>
2024-06-07 18:42:48 +0200

    Delete porject directory

--- porject/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // Démarrer la session
-session_unset(); // Libérer toutes les variables de session
-session_destroy(); // Détruire la session
-
-header("Location: login.html"); // Rediriger vers la page de connexion
-exit;
-?>
